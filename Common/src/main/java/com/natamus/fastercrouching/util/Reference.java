967014b90a90ff36ffb550c6a70cc40690442b8b
Rick South <rick@natamus.com>
2023-12-12 10:15:59 +0100

    Faster Crouching source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/fastercrouching/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.fastercrouching.util;
+
+public class Reference {
+	public static final String MOD_ID = "fastercrouching";
+	public static final String NAME = "Faster Crouching";
+	public static final String VERSION = "2.2";
+	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
+}
