624067cad8ac79ec02aa3d428b71d63bb5381d11
Rick South <rick@natamus.com>
2023-12-12 10:15:55 +0100

    Faster Crouching source code for 1.18.2.

---
+++ Fabric/src/main/java/com/natamus/fastercrouching/fabric/mixin/KeyboardInputMixin.java
@@ -0,0 +1,19 @@
+package com.natamus.fastercrouching.fabric.mixin;
+
+import net.minecraft.client.player.Input;
+import net.minecraft.client.player.KeyboardInput;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = KeyboardInput.class, priority = 1001)
+public class KeyboardInputMixin extends Input {
+    @Inject(method = "tick(Z)V", at = @At(value = "TAIL"))
+    public void tick(boolean pIsMovingSlowly, CallbackInfo ci) {
+        if (pIsMovingSlowly) {
+            this.leftImpulse *= 10.0F;
+            this.forwardImpulse *= 10.0F;
+        }
+    }
+}
