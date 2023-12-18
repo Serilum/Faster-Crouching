c57ed9cf6284292eaa1e8ac008855fddc62fe4cf
Rick South <rick@natamus.com>
2023-12-18 10:29:17 +0100

    (v2.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/fastercrouching/util/Reference.java
+++ Common/src/main/java/com/natamus/fastercrouching/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.fastercrouching.util;
 public class Reference {
 	public static final String MOD_ID = "fastercrouching";
 	public static final String NAME = "Faster Crouching";
-	public static final String VERSION = "2.2";
+	public static final String VERSION = "2.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
