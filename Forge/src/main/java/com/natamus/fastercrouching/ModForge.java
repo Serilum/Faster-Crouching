4eb2d1991ce6263e29896c6cb999d408c216a267
Rick South <rick@natamus.com>
2024-05-01 16:41:04 +0200

    Faster Crouching source code for 1.20.6.

---
+++ Forge/src/main/java/com/natamus/fastercrouching/ModForge.java
@@ -0,0 +1,30 @@
+package com.natamus.fastercrouching;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.fastercrouching.util.Reference;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
