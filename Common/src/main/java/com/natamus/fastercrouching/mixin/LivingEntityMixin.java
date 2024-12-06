dc0f7db4784a10ecae42f0cb4f7410ef188a4b3b
Rick South <rick@natamus.com>
2024-12-06 14:19:18 +0100

    (v2.7) (1.21.4) Fixed a mixin target error.

--- Common/src/main/java/com/natamus/fastercrouching/mixin/LivingEntityMixin.java
+++ Common/src/main/java/com/natamus/fastercrouching/mixin/LivingEntityMixin.java
@@ -1,12 +1,16 @@
 package com.natamus.fastercrouching.mixin;
 
+import net.minecraft.core.Holder;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = LivingEntity.class, priority = 1001)
 public class LivingEntityMixin {
@@ -21,6 +25,13 @@ public class LivingEntityMixin {
 				this.speed = pSpeed * 10.0F;
 				ci.cancel();
 			}
+		}
+	}
+
+	@Inject(method = "getAttributeValue(Lnet/minecraft/core/Holder;)D", at = @At(value = "HEAD"), cancellable = true)
+	public void getAttributeValue(Holder<Attribute> attributeHolder, CallbackInfoReturnable<Double> cir) {
+		if (attributeHolder.equals(Attributes.SNEAKING_SPEED)) {
+			cir.setReturnValue((double) ((0.3F + ((LivingEntity)(Object)this).getAttributes().getValue(Attributes.SNEAKING_SPEED)) * 10.0F));
 		}
 	}
 }
